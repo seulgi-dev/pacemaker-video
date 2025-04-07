b66975eb0b37b9236a5ad860b5d6ec08cf691d1d
Mirim Kim <mirimkim00@gmail.com>
2025-04-07 15:03:27 -0400

    PACE-30 add admin role

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,23 @@
-export default function AdminDashboard() {
+import { auth } from '@clerk/nextjs/server';
+import { createClerkClient } from '@clerk/backend';
+import { redirect } from 'next/navigation';
+
+export default async function AdminDashboard() {
+  const clerkClient = createClerkClient({
+    secretKey: process.env.CLERK_SECRET_KEY
+  });
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const user = await clerkClient.users.getUser(userId);
+  const role = user.publicMetadata.role;
+
+  if (role !== 'admin') {
+    redirect('/');
+  }
+
   return <div>AdminDashboard</div>;
 }
