4ea3e29c609b2925b7a101d6b44cc7c456745d2b
ljh981009 <ljh981009@naver.com>
2025-06-05 20:02:58 -0400

    create main layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,6 +32,31 @@ export default async function Home() {
         }}
       />
       <div className="w-[1200px] items-center mx-auto justify-center flex flex-col gap-8">
+        <iframe
+          width="1200"
+          height="673"
+          src="https://www.youtube.com/embed/gk43OcYMes8?si=4rPZKvzBGS9H6Mhx&mute=1&autoplay=1"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
+        <h1 className="text-pace-5xl font-bold pt-2 ">
+          {'성공한 선배들이 전하는 진짜 북미 취업 이야기'}
+        </h1>
+        <span className="font-light text-pace-sm text-center whitespace-pre-line leading-[140%]">
+          {
+            '어렵기만 한 북미 취업, 어디서부터 어떻게 시작해야 할지 막막하셨죠?\n페이스메이커 온라인 강의 플랫폼에서는 IT, 그래픽 디자인, UX/UI 디자인,마케팅, 회계 등 다양한 분야에서\n 실제 북미 기업 취업에 성공한 멘토들이 실전 노하우를 전해드립니다.\n \n 지금 한국에 계시든, 캐나다에계시든 장소에 구애받지 않고\n 페이스메이커와 함께 북미 취업에 도전해보세요!\n'
+          }
+        </span>
+        <div className="flex justify-center items-center gap-4">
+          <Button className="h-12 bg-white text-pace-orange-600 border border-pace-orange-600 p-4 rounded-full flex justify-center items-center mx-auto font-normal ">
+            {'온라인 강의 둘러보기'}
+          </Button>
+          <Button className="h-12 bg-pace-orange-600 text-white border border-pace-orange-600 p-4 rounded-full flex justify-center items-center mx-auto font-normal ">
+            {'로그인하고 강의 듣기'}
+          </Button>
+        </div>
         <VideoList />
         <UserInfo />
       </div>
