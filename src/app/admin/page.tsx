f8f3bc285a15f781ffd438044b44562223f7b911
Mirim Kim <mirimkim00@gmail.com>
2025-06-12 16:37:43 -0400

    PACE-58 Fix unauthorized access issue on admin page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,28 +1,7 @@
-import { auth } from '@clerk/nextjs/server';
-import { redirect } from 'next/navigation';
-import { createClientSupabase } from '@/lib/supabase';
-
 export default async function AdminDashboard() {
-  const { userId } = await auth();
-
-  const supabase = createClientSupabase();
-  if (!userId) {
-    redirect('/sign-in');
-  }
-
-  const { data: currentUser, error } = await supabase
-    .from('UserPublicView')
-    .select('role')
-    .eq('clerkId', userId)
-    .single();
-
-  if (error || !currentUser) {
-    redirect('/');
-  }
-
-  if (currentUser.role !== 'ADMIN') {
-    redirect('/');
-  }
-
-  return <div>AdminDashboard</div>;
+  return (
+    <>
+      <div>admin page</div>
+    </>
+  );
 }
