d27f4dcfde73e93b04f903d533dca797b4dab0b9
Sehwan <sehwan.lee.dev@gmail.com>
2025-06-23 22:22:28 -0300

    Add Card component stories and include addon-designs in Storybook configuration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from '@storybook/nextjs';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@chromatic-com/storybook', '@storybook/addon-docs'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    '@storybook/addon-designs'
+  ],
   framework: {
     name: '@storybook/nextjs',
     options: {}
