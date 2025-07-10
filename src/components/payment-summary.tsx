68597017719e3a215948580c200cafa44ced6599
Mirim Kim <mirimkim00@gmail.com>
2025-07-10 18:19:01 -0400

    Implemented sticky payment summary

--- src/components/payment-summary.tsx
+++ src/components/payment-summary.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image';
+import { useState } from 'react';
+import { Button } from './ui/button';
 import { CartItem } from '@/types/my-card';
 
 interface PaymentSummaryProps {
@@ -5,6 +8,8 @@ interface PaymentSummaryProps {
 }
 
 export default function PaymentSummary({ cartItems }: PaymentSummaryProps) {
+  const [showDetails, setShowDetails] = useState(false);
+
   const selectedItem = cartItems.filter((item) => item.selected);
   const subtotal = selectedItem.reduce((acc, item) => acc + item.price, 0);
   const discount = 20;
@@ -12,44 +17,122 @@ export default function PaymentSummary({ cartItems }: PaymentSummaryProps) {
   const total = subtotal - discount + tax;
 
   return (
-    <aside className="w-80 h-full border-l p-10 pt-20">
-      <h2 className="text-lg font-bold mb-4">예상 결제 금액</h2>
-      <ul className="space-y-4 mb-6 font-normal border-b border-pace-gray-700 pb-6 text-pace-base text-pace-gray-700">
-        <li className="flex justify-between">
-          <span className="text-[#6B7280]">
-            소계 ({selectedItem.length}개의 강의)
-          </span>
-          <span>${subtotal.toFixed(2)}</span>
-        </li>
-        <li className="flex justify-between">
-          <span className="text-[#6B7280]">할인 금액</span>
-          <span>-${discount.toFixed(2)}</span>
-        </li>
-        <li className="flex justify-between">
-          <span className="text-[#6B7280]">세금</span>
-          <span>${tax.toFixed(2)}</span>
-        </li>
-      </ul>
-      <div className="flex justify-between text-pace-base font-semibold">
-        <span className="text-pace-gray-700">총 결제 금액</span>
-        <span className="text-[#E86642]">${total.toFixed(2)}</span>
-      </div>
-      <div className="w-60 h-[37px] flex gap-1 my-6 text-pace-sm">
-        <input
-          type="text"
-          placeholder="프로모션 코드 입력"
-          className="flex-1 min-w-0 px-3 py-1 placeholder-[#757575] rounded-full border border-[#EEEEEE] focus:border-[#6F6F6F] focus:outline-none"
-        />
-        <button className="w-[57px] px-3 py-1 text-pace-gray-700 rounded-full border border-[#EEEEEE] hover:border-[#6F6F6F]">
-          등록
+    <>
+      <aside className="hidden lg:block w-80 h-full border-l p-10 pt-20">
+        <h2 className="text-lg font-bold mb-4">예상 결제 금액</h2>
+        <ul className="space-y-4 mb-6 font-normal border-b border-pace-gray-700 pb-6 text-pace-base text-pace-gray-700">
+          <li className="flex justify-between">
+            <span className="text-[#6B7280]">
+              소계 ({selectedItem.length}개의 강의)
+            </span>
+            <span>${subtotal.toFixed(2)}</span>
+          </li>
+          <li className="flex justify-between">
+            <span className="text-[#6B7280]">할인 금액</span>
+            <span>-${discount.toFixed(2)}</span>
+          </li>
+          <li className="flex justify-between">
+            <span className="text-[#6B7280]">세금</span>
+            <span>${tax.toFixed(2)}</span>
+          </li>
+        </ul>
+        <div className="flex justify-between text-pace-base font-semibold">
+          <span className="text-pace-gray-700">총 결제 금액</span>
+          <span className="text-[#E86642] font-bold">${total.toFixed(2)}</span>
+        </div>
+        <div className="w-60 h-[37px] flex gap-1 my-6 text-pace-sm">
+          <input
+            type="text"
+            placeholder="프로모션 코드 입력"
+            className="flex-1 min-w-0 px-3 py-1 placeholder-[#757575] rounded-full border border-[#EEEEEE] focus:border-[#6F6F6F] focus:outline-none"
+          />
+          <button className="w-[57px] px-3 py-1 text-pace-gray-700 rounded-full border border-[#EEEEEE] hover:border-[#6F6F6F]">
+            등록
+          </button>
+        </div>
+        <button className="w-full h-[56px] bg-orange-500 text-white py-2 rounded-full">
+          결제하기
         </button>
-      </div>
-      <button className="w-full h-[56px] bg-orange-500 text-white py-2 rounded-full">
-        결제하기
-      </button>
-      <div className="flex justify-center mt-6 text-pace-stone-700 text-[12px]">
-        Secure payment powered by Stripe
-      </div>
-    </aside>
+        <div className="flex justify-center mt-6 text-pace-stone-700 text-[12px]">
+          Secure payment powered by Stripe
+        </div>
+      </aside>
+
+      {/* Sticky Payment Summary */}
+      <aside className="block lg:hidden fixed bottom-0 left-0 right-0 bg-white border-t border-pace-orange-600 py-4 z-10">
+        <div className="flex flex-col items-center">
+          {/* 상세보기 버튼 */}
+          <div className="flex flex-col items-center text-pace-sm text-pace-stone-800 font-light mb-4">
+            <Button
+              variant="ghost"
+              size="lg"
+              onClick={() => setShowDetails(!showDetails)}
+            >
+              {showDetails ? '닫기' : '상세보기'}
+              <Image
+                src={
+                  showDetails
+                    ? '/icons/chevron-down.svg'
+                    : '/icons/chevron-up.svg'
+                }
+                alt=""
+                width={24}
+                height={24}
+              />
+            </Button>
+          </div>
+
+          {/* 상세보기 내용 */}
+          {showDetails && (
+            <div className="w-2/3 text-pace-base text-pace-gray-700">
+              <h2 className="text-pace-gray-500 text-pace-lg font-bold mb-4">
+                예상 결제 금액
+              </h2>
+              <ul className="space-y-4 font-normal text-pace-base text-pace-gray-700">
+                <li className="flex justify-between">
+                  <span className="text-[#6B7280]">
+                    소계 ({selectedItem.length}개의 강의)
+                  </span>
+                  <span>${subtotal.toFixed(2)}</span>
+                </li>
+                <li className="flex justify-between">
+                  <span className="text-[#6B7280]">할인 금액</span>
+                  <span>-${discount.toFixed(2)}</span>
+                </li>
+                <li className="flex justify-between">
+                  <span className="text-[#6B7280]">세금</span>
+                  <span>${tax.toFixed(2)}</span>
+                </li>
+              </ul>
+              <div className="border-b border-pace-gray-700 mb-4">
+                <div className="w-60 h-[37px] flex gap-1 my-6 text-pace-sm ml-auto">
+                  <input
+                    type="text"
+                    placeholder="프로모션 코드 입력"
+                    className="flex-1 min-w-0 px-3 py-1 placeholder-[#757575] rounded-full border border-[#EEEEEE] focus:border-[#6F6F6F] focus:outline-none"
+                  />
+                  <button className="w-[57px] px-3 py-1 text-pace-gray-700 rounded-full border border-[#EEEEEE] hover:border-[#6F6F6F]">
+                    등록
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+
+          {/* 총 금액 & 결제 버튼 */}
+          <div className="w-2/3 flex justify-between items-center text-pace-xl">
+            <span className="font-medium">총 결제 금액</span>
+            <div className="flex items-center gap-4">
+              <span className="text-[#E86642] font-bold">
+                ${total.toFixed(2)}
+              </span>
+              <button className="w-60 h-[56px] bg-orange-500 text-white px-4 py-2 rounded-full text-pace-lg">
+                결제하기
+              </button>
+            </div>
+          </div>
+        </div>
+      </aside>
+    </>
   );
 }
